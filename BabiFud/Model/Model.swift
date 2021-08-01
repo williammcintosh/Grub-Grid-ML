793d43bdaa0a48cc98a5d3f7c4a80002cf18f329
Will McIntosh <william.michael.mcintosh@gmail.com>
2021-08-01 12:52:16 -0700

    Upading the Model.swift file to be able to handle the query with collaborative filtering. It's still a work in progress.

--- BabiFud/Model/Model.swift
+++ BabiFud/Model/Model.swift
@@ -24,67 +24,85 @@ class Model {
   }
   
   @objc func refresh(_ completion: @escaping (Error?) -> Void) {
+    var queries = [CKQuery] ()
     let nu_recipes: [Int] = [254921,361650,215716,248350]
     let nu_ratings: [Int] = [0,1,0,1]
     var sim_user_id: Int64 = 0
     if (nu_ratings.count % 2 == 0) && (nu_recipes.count % 2 == 0) {
       sim_user_id = GetCosineSimilarity(rec:nu_recipes,rat:nu_ratings)
     }
-    
     // RETURNS NAME SEARCH:
     //let searchText = "arriba   baked winter squash mexican style"
     //let predicate = NSPredicate(format: "name == %@",searchText)
     
-    // OR Predicate for the first word with 'OR' without the space:
+    let likedQuery = GetRecipesWithLikedIds()
+    queries.append(likedQuery)
+    let unlikedQuery = GetRecipesWithoutLikedIds()
+    queries.append(unlikedQuery)
+
+    //SORTS LIST BY RECIPE_ID
+    //let sort = NSSortDescriptor(key: "recipe_id", ascending: false)
+    //query.sortDescriptors = [sort]
+    AttachToMainThread(forQuery: queries, completion)
+  }
+  //Separating this into a different function is just for readability
+  private func AttachToMainThread(forQuery queries: [CKQuery],
+      _ completion: @escaping (Error?) -> Void) {
+    for q in queries {
+      publicDB.perform(q,
+          inZoneWith: CKRecordZone.default().zoneID) { [weak self] results, error in
+        guard let self = self else { return }
+        if let error = error {
+          DispatchQueue.main.async {
+            completion(error)
+          }
+          return
+        }
+        guard let results = results else { return }
+        self.recipes = results.compactMap {
+          Recipe(record: $0, database: self.publicDB)
+          
+        }
+        for r in self.recipes {
+          self.PrettyPrintRecipes(rName: r.name, rId: String(r.recipe_id), rIng: r.ingredients)
+        }
+        DispatchQueue.main.async {
+          completion(nil)
+        }
+      }
+    }
+  }
+  
+  func GetRecipesWithLikedIds() -> CKQuery {
     let searchTextA: [String] = [carbohydrate," "+carbohydrate,carbohydrate+"s",carbohydrate+"es"]
     let subPred1 = NSPredicate (format: "ANY ingredients IN %@",argumentArray: [searchTextA])
-    
     // OR Predicate for the first word with 'OR' without the space:
     let searchTextB: [String] = [vegetable," "+vegetable,vegetable+"s",vegetable+"es"]
     let subPred2 = NSPredicate (format: "ANY ingredients IN %@",argumentArray: [searchTextB])
-    
+    // Attempting to prioritize recipe_id 13733
+    let priorityIds: [Int] = [13733,32441]
+    let subPred3 = NSPredicate (format: "NOT (recipe_id IN %@)", priorityIds)
     // AND compound predicate combing both of the ones above:
-    let predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [subPred1, subPred2])
-
-    // I'm in the works of grabbing recipes from the same user_id, but having a challenging time doing so.
-    //fetchRequest.sortDescriptors = [NSSortDescriptor(key: "section", ascending: true),// sort by section key first
-//                                     NSSortDescriptor(key:  "Name", ascending: true), //continue sorting by first name
-//                                     NSSortDescriptor(key:  "LastName", ascending: true), //continue sorting by last name.
-//                                     NSSortDescriptor(key: "Place", ascending: true)] //finally by place.
-    let query = CKQuery(recordType: "Recipe", predicate: predicate)
-    if (sim_user_id > 0) {
-      let sim_user = NSSortDescriptor(key: "recipe_id", ascending: false)
-      query.sortDescriptors = [sim_user]
-    } else {
-      let sort = NSSortDescriptor(key: "recipe_id", ascending: false)
-      query.sortDescriptors = [sort]
-    }
-    AttachToMainThread(forQuery: query, completion)
+    let and_pred1 = NSCompoundPredicate(andPredicateWithSubpredicates: [subPred1, subPred2])
+    let and_pred2 = NSCompoundPredicate(andPredicateWithSubpredicates: [subPred3])
+    let predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [and_pred1, and_pred2])
+    return CKQuery(recordType: "Recipe", predicate: predicate)
   }
-  //Separating this into a different function isn't necessary
-  private func AttachToMainThread(forQuery query: CKQuery,
-      _ completion: @escaping (Error?) -> Void) {
-    publicDB.perform(query,
-        inZoneWith: CKRecordZone.default().zoneID) { [weak self] results, error in
-      guard let self = self else { return }
-      if let error = error {
-        DispatchQueue.main.async {
-          completion(error)
-        }
-        return
-      }
-      guard let results = results else { return }
-      self.recipes = results.compactMap {
-        Recipe(record: $0, database: self.publicDB)
-        
-      }
-      for r in self.recipes {
-        self.PrettyPrintRecipes(rName: r.name, rId: String(r.recipe_id), rIng: r.ingredients)
-      }
-      DispatchQueue.main.async {
-        completion(nil)
-      }
-    }
+  
+  func GetRecipesWithoutLikedIds() -> CKQuery {
+    let searchTextA: [String] = [carbohydrate," "+carbohydrate,carbohydrate+"s",carbohydrate+"es"]
+    let subPred1 = NSPredicate (format: "ANY ingredients IN %@",argumentArray: [searchTextA])
+    // OR Predicate for the first word with 'OR' without the space:
+    let searchTextB: [String] = [vegetable," "+vegetable,vegetable+"s",vegetable+"es"]
+    let subPred2 = NSPredicate (format: "ANY ingredients IN %@",argumentArray: [searchTextB])
+    // Attempting to prioritize recipe_id 13733
+    let priorityIds: [Int] = [13733,32441]
+    let subPred3 = NSPredicate (format: "recipe_id IN %@",argumentArray: [priorityIds])
+    // AND compound predicate combing both of the ones above:
+    let and_pred1 = NSCompoundPredicate(andPredicateWithSubpredicates: [subPred1, subPred2])
+    let and_pred2 = NSCompoundPredicate(andPredicateWithSubpredicates: [subPred3])
+    let predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [and_pred1, and_pred2])
+    return CKQuery(recordType: "Recipe", predicate: predicate)
   }
 
   func PrettyPrintRecipes(rName: String, rId: String, rIng: [String]) {
