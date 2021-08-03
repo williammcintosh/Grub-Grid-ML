94847271dff30e56a7be02147bb72e5a615fc7bc
Will McIntosh <william.michael.mcintosh@gmail.com>
2021-08-03 12:50:36 -0700

    trying to get images to work

--- BabiFud/Model/Model.swift
+++ BabiFud/Model/Model.swift
@@ -151,4 +151,35 @@ class Model {
   public func printVegetable(){
     print(vegetable)
   }
+  
+  func GetImageLink(searchResult: String) -> String{
+    var returnStr: String = ""
+    print("NAME = "+searchResult)
+    let url = URL(string: "https://grubgridimagesearch.herokuapp.com/grabimage/"+searchResult)
+  
+    guard let requestUrl = url else { fatalError() }
+    // Create URL Request
+    var request = URLRequest(url: requestUrl)
+    // Specify HTTP Method to use
+    request.httpMethod = "GET"
+    // Send HTTP Request
+    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+        // Check if Error took place
+        if let error = error {
+            print("Error took place \(error)")
+            return
+        }
+        // Read HTTP Response Status code
+        if let response = response as? HTTPURLResponse {
+            print("Response HTTP Status code: \(response.statusCode)")
+        }
+        // Convert HTTP Response Data to a simple String
+        if let data = data, let dataString = String(data: data, encoding: .utf8) {
+            print("Response data string:\n \(dataString)")
+          returnStr = dataString
+        }
+    }
+    task.resume()
+    return returnStr
+  }
 }
--- BabiFud/Model/Recipe.swift
+++ BabiFud/Model/Recipe.swift
@@ -26,43 +26,14 @@ class Recipe {
     let newName: String = record["name"] as? String ?? ""
     self.name = newName
     self.database = database
-    //self.recipeURL = "https://www.mcdonalds.com/is/image/content/dam/uk/nfl/nutrition/nfl-product/product/products/mcdonalds-Big-Mac.jpg"
-    self.recipeURL = GetImageLink(searchResult: newName)
+    self.recipeURL = "https://www.mcdonalds.com/is/image/content/dam/uk/nfl/nutrition/nfl-product/product/products/mcdonalds-Big-Mac.jpg"
+    //self.recipeURL = Model.currentModel.GetImageLink(searchResult: newName)
     //self.description = record["description"] as? String ?? ""
     self.recipe_id = record["recipe_id"] as? Int64 ?? 0
     self.ingredients = record["ingredients"] as? [String] ?? [""]
     //self.minutes = record["minutes"] as? Int64 ?? 0
     self.steps = record["steps"] as? [String] ?? [""]
     //self.n_steps = Int64(steps.count)
-  }
-  
-  func GetImageLink(searchResult: String) -> String{
-    var returnStr: String = ""
-    let url = URL(string: "https://grubgridimagesearch.herokuapp.com/grabimage/"+searchResult)
-    guard let requestUrl = url else { fatalError() }
-    // Create URL Request
-    var request = URLRequest(url: requestUrl)
-    // Specify HTTP Method to use
-    request.httpMethod = "GET"
-    // Send HTTP Request
-    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-        // Check if Error took place
-        if let error = error {
-            print("Error took place \(error)")
-            return
-        }
-        // Read HTTP Response Status code
-        if let response = response as? HTTPURLResponse {
-            print("Response HTTP Status code: \(response.statusCode)")
-        }
-        // Convert HTTP Response Data to a simple String
-        if let data = data, let dataString = String(data: data, encoding: .utf8) {
-            print("Response data string:\n \(dataString)")
-          returnStr = dataString
-        }
-    }
-    task.resume()
-    return returnStr
   }
 }
 
